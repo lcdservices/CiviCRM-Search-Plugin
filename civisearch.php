7ff5a2fe80f3c2aafde7ea5dad513dc5ce7c7b52
Marek Handze <marek@dell.(none)>
2011-08-18 16:28:08 +0200

    first working version

--- civisearch.php
+++ civisearch.php
@@ -21,7 +21,7 @@ require_once JPATH_SITE.'/components/com_content/router.php';
  * @subpackage  Search.content
  * @since       1.6
  */
-class plgCiviSearch extends JPlugin
+class plgSearchCiviSearch extends JPlugin
 {
     /**
      * @return array An array of search areas
@@ -63,177 +63,66 @@ class plgCiviSearch extends JPlugin
         $sArchived      = $this->params->get('search_archived',     1);
         $limit          = $this->params->def('search_limit',        50);
 
-        $nullDate       = $db->getNullDate();
-        $date = JFactory::getDate();
-        $now = $date->toMySQL();
+        $state          = array();
+        if ($sContent) {
+            $state[]=1;
+        }
+        if ($sArchived) {
+            $state[]=2;
+        }
+
 
         $text = trim($text);
         if ($text == '') {
             return array();
         }
 
-        $wheres = array();
-        switch ($phrase) {
-            case 'exact':
-                $text       = $db->Quote('%'.$db->getEscaped($text, true).'%', false);
-                $wheres2    = array();
-                $wheres2[]  = 'a.title LIKE '.$text;
-                $wheres2[]  = 'a.introtext LIKE '.$text;
-                $wheres2[]  = 'a.fulltext LIKE '.$text;
-                $wheres2[]  = 'a.metakey LIKE '.$text;
-                $wheres2[]  = 'a.metadesc LIKE '.$text;
-                $where      = '(' . implode(') OR (', $wheres2) . ')';
-                break;
-
-            case 'all':
-            case 'any':
-            default:
-                $words = explode(' ', $text);
-                $wheres = array();
-                foreach ($words as $word) {
-                    $word       = $db->Quote('%'.$db->getEscaped($word, true).'%', false);
-                    $wheres2    = array();
-                    $wheres2[]  = 'a.title LIKE '.$word;
-                    $wheres2[]  = 'a.introtext LIKE '.$word;
-                    $wheres2[]  = 'a.fulltext LIKE '.$word;
-                    $wheres2[]  = 'a.metakey LIKE '.$word;
-                    $wheres2[]  = 'a.metadesc LIKE '.$word;
-                    $wheres[]   = implode(' OR ', $wheres2);
-                }
-                $where = '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
-                break;
-        }
-
-        $morder = '';
         switch ($ordering) {
-            case 'oldest':
-                $order = 'a.created ASC';
-                break;
-
-            case 'popular':
-                $order = 'a.hits DESC';
-                break;
-
             case 'alpha':
                 $order = 'a.title ASC';
                 break;
 
             case 'category':
-                $order = 'c.title ASC, a.title ASC';
-                $morder = 'a.title ASC';
-                break;
-
+            case 'popular':
             case 'newest':
+            case 'oldest':
             default:
-                $order = 'a.created DESC';
-                break;
+                $order = 'a.title DESC';
         }
 
-        $rows = array();
+        $text   = $db->Quote('%'.$db->getEscaped($text, true).'%', false);
         $query  = $db->getQuery(true);
 
-        // search articles
-        if ($sContent && $limit > 0)
-        {
-            $query->clear();
-            $query->select('a.title AS title, a.metadesc, a.metakey, a.created AS created, '
-                        .'CONCAT(a.introtext, a.fulltext) AS text, c.title AS section, '
-                        .'CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(":", a.id, a.alias) ELSE a.id END as slug, '
-                        .'CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(":", c.id, c.alias) ELSE c.id END as catslug, '
-                        .'"2" AS browsernav');
-            $query->from('#__content AS a');
-            $query->innerJoin('#__categories AS c ON c.id=a.catid');
-            $query->where('('. $where .')' . 'AND a.state=1 AND c.published = 1 AND a.access IN ('.$groups.') '
-                        .'AND c.access IN ('.$groups.') '
-                        .'AND (a.publish_up = '.$db->Quote($nullDate).' OR a.publish_up <= '.$db->Quote($now).') '
-                        .'AND (a.publish_down = '.$db->Quote($nullDate).' OR a.publish_down >= '.$db->Quote($now).')' );
+        $return = array();
+        if (!empty($state)) {
+            $query->select('a.title, a.description AS text, a.created_date AS created, "2" AS browsernav, a.id AS eventid');
+            $query->from('civicrm_event AS a');
+            $query->where('(a.title LIKE '. $text .' OR a.description LIKE '. $text .')');
             $query->group('a.id');
             $query->order($order);
-
-            // Filter by language
             if ($app->isSite() && $app->getLanguageFilter()) {
-                $query->where('a.language in (' . $db->Quote($tag) . ',' . $db->Quote('*') . ')');
-                $query->where('c.language in (' . $db->Quote($tag) . ',' . $db->Quote('*') . ')');
+                $query->where('a.language in (' . $db->Quote(JFactory::getLanguage()->getTag()) . ',' . $db->Quote('*') . ')');
             }
 
             $db->setQuery($query, 0, $limit);
-            $list = $db->loadObjectList();
-            $limit -= count($list);
+            $rows = $db->loadObjectList();
 
-            if (isset($list))
-            {
-                foreach($list as $key => $item)
-                {
-                    $list[$key]->href = ContentHelperRoute::getArticleRoute($item->slug, $item->catslug);
+            if ($rows) {
+                $count = count($rows);
+                for ($i = 0; $i < $count; $i++) {
+                    $rows[$i]->href = ContentHelperRoute::getCategoryRoute($rows[$i]->slug);
+                    $rows[$i]->href = 'index.php?option=com_civicrm&task=civicrm/event/info&reset=1&id='.$rows[$i]->eventid;
+                    $rows[$i]->section  = JText::_('CiviCRM Event');
                 }
-            }
-            $rows[] = $list;
-        }
 
-        // search archived content
-        if ($sArchived && $limit > 0)
-        {
-            $searchArchived = JText::_('JARCHIVED');
-
-            $query->clear();
-            $query->select('a.title AS title, a.metadesc, a.metakey, a.created AS created, '
-                        .'CONCAT(a.introtext, a.fulltext) AS text, '
-                        .'CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(":", a.id, a.alias) ELSE a.id END as slug, '
-                        .'CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(":", c.id, c.alias) ELSE c.id END as catslug, '
-                        .'CONCAT_WS("/", c.title) AS section, "2" AS browsernav' );
-            $query->from('#__content AS a');
-            $query->innerJoin('#__categories AS c ON c.id=a.catid AND c.access IN ('. $groups .')');
-            $query->where('('. $where .') AND a.state = 2 AND c.published = 1 AND a.access IN ('. $groups
-                .') AND c.access IN ('. $groups .') '
-                .'AND (a.publish_up = '.$db->Quote($nullDate).' OR a.publish_up <= '.$db->Quote($now).') '
-                .'AND (a.publish_down = '.$db->Quote($nullDate).' OR a.publish_down >= '.$db->Quote($now).')' );
-            $query->order($order);
-
-
-            // Filter by language
-            if ($app->isSite() && $app->getLanguageFilter()) {
-                $query->where('a.language in (' . $db->Quote($tag) . ',' . $db->Quote('*') . ')');
-                $query->where('c.language in (' . $db->Quote($tag) . ',' . $db->Quote('*') . ')');
-            }
-
-            $db->setQuery($query, 0, $limit);
-            $list3 = $db->loadObjectList();
-
-            // find an itemid for archived to use if there isn't another one
-            $item   = $app->getMenu()->getItems('link', 'index.php?option=com_content&view=archive', true);
-            $itemid = isset($item) ? '&Itemid='.$item->id : '';
-
-            if (isset($list3))
-            {
-                foreach($list3 as $key => $item)
-                {
-                    $date = JFactory::getDate($item->created);
-
-                    $created_month  = $date->format("n");
-                    $created_year   = $date->format("Y");
-
-                    $list3[$key]->href  = JRoute::_('index.php?option=com_content&view=archive&year='.$created_year.'&month='.$created_month.$itemid);
-                }
-            }
-
-            $rows[] = $list3;
-        }
-
-        $results = array();
-        if (count($rows))
-        {
-            foreach($rows as $row)
-            {
-                $new_row = array();
-                foreach($row AS $key => $article) {
-                    if (searchHelper::checkNoHTML($article, $searchText, array('text', 'title', 'metadesc', 'metakey'))) {
-                        $new_row[] = $article;
+                $return = array();
+                foreach($rows AS $key => $category) {
+                    if (searchHelper::checkNoHTML($category, $searchText, array('name', 'title', 'text'))) {
+                        $return[] = $category;
                     }
                 }
-                $results = array_merge($results, (array) $new_row);
             }
         }
-
-        return $results;
+        return $return;
     }
 }
